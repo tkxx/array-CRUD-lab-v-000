0310136cfeb84040beccb8bb4c294f681ef67817
Tabitha Sin <tabitha.sin@gmail.com>
2017-10-12 17:30:41 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   sokka = ["no", "way", "am", "i", "a bender"]
-  aang[2]
+  sokka[2]
 end
 
 def retrieve_first_element_from_array(array)
