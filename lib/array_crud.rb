a70995bc71214fde8e871257bb666ae7b2f447c2
Tabitha Sin <tabitha.sin@gmail.com>
2017-10-12 17:32:18 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  team_avatar = ["earthbender", "firebender", "waterbender", "airbender"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  team_avatar = ["earthbender", "firebender", "waterbender", "airbender"]
+  team_avatar << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  team_avatar = ["earthbender", "firebender", "waterbender", "airbender"]
+  team_avatar.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  team_avatar = ["earthbender", "firebender", "waterbender", "airbender", "arrays!"]
+  array = team_avatar.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  team_avatar = ["wow", "earthbender", "firebender", "waterbender", "airbender"]
+  wow = team_avatar.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  sokka = ["no", "way", "am", "i", "a bender"]
+  sokka[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  team_avatar = ["wow", "earthbender", "firebender", "waterbender", "airbender"]
+  team_avatar.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  team_avatar = ["earthbender", "firebender", "waterbender", "airbender", "arrays!"]
+  team_avatar.last
 end
